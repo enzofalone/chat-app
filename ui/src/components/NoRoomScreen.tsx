12d35940d1f89d6e7f10e26fb458bd7faee2c851
Enzo Falone <enzofalone123@gmail.com>
2023-03-06 10:43:18 -0500

    removed old commented code

--- ui/src/components/NoRoomScreen.tsx
+++ ui/src/components/NoRoomScreen.tsx
@@ -34,26 +34,6 @@ const NoRoomScreen: FC<Props> = ({
         ) : (
           <></>
         )}
-        {/*   <form onSubmit={handleOnSubmitUsername}>
-          <input
-            className="rounded-md p-3 text-lg "
-            disabled={isUsernameDone}
-            type={"text"}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder={"Username"}
-          />
-          {!isUsernameDone ? (
-            <input
-              type={"submit"}
-              className={
-                "p-3 mx-5 text-lg rounded-md bg-green-600 cursor-pointer hover:bg-green-700 ease-in-out"
-              }
-            />
-          ) : (
-            <></>
-          )}
-        </form> */}
       </div>
     </div>
   );
