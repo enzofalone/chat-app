ef63142416875d9468a4c979fb0ab3151647e8bf
Enzo Falone <enzofalone123@gmail.com>
2023-07-20 00:39:09 -0400

    fixed spacing

--- ui/src/components/Chat/ChatHeader/ChatHeader.tsx
+++ ui/src/components/Chat/ChatHeader/ChatHeader.tsx
@@ -7,10 +7,10 @@ type Props = {
 
 const ChatHeader: FC<Props> = ({ title }: Props) => {
   return (
-    <div className="w-full flex justify-start py-3 border-b-[1px] align-middle border-gray-700 bg-[#1c1c24] min-h-[52px] max-h-[52px]">
+    <div className="w-full flex justify-start py-3 border-b-[1px] align-middle border-gray-700 bg-[#1c1c24] min-h-[60px] max-h-[60px]">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="icon icon-tabler icon-tabler-hash ml-2"
+        className="icon icon-tabler icon-tabler-hash ml-3 my-auto"
         width="24"
         height="24"
         viewBox="0 0 24 24"
@@ -26,7 +26,7 @@ const ChatHeader: FC<Props> = ({ title }: Props) => {
         <path d="M11 4l-4 16"></path>
         <path d="M17 4l-4 16"></path>
       </svg>
-      <span className="text-middle text-lg font-bold self-baseline align-middle leading-snug">
+      <span className="text-middle text-lg font-bold self-baseline align-middle leading-snug my-auto">
         {title || ""}
       </span>
       <LogOutButton/>
